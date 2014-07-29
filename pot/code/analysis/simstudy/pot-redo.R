a608f177f570bae8548e014b7fe527566c169c47
Sam Morris <sam.morris81@gmail.com>
2014-07-29 15:50:08 -0500

    minor tweak to pot redo, new presentation

--- pot/code/analysis/simstudy/pot-redo.R
+++ pot/code/analysis/simstudy/pot-redo.R
@@ -507,3 +507,124 @@ g <- 4
   rm(fit.1)
   gc()
 # }
+
+#### Load simdata
+rm(list = ls())
+load(file='./simdata.RData')
+source('../../R/mcmc.R')
+source('../../R/auxfunctions.R')
+
+setting <- 4
+analysis <- "a"
+iters <- 20000; burn <- 10000; update <- 1000; thin <- 1
+nsets <- 5
+
+# for (g in 1) {
+g <- 5
+  # fit.1 <- vector(mode="list", length=nsets)
+  # y.validate <- array(NA, dim=c(ntest, nt, nsets))
+  outputfile <- paste(setting, "-", analysis, "-", g, ".RData", sep="")
+  load(outputfile)
+  
+  start <- proc.time()
+  # for (d in 1:nsets) {
+  d <- 3
+    dataset <- (g-1) * 5 + d
+    cat("start dataset", dataset, "\n")
+    set.seed(setting * 100 + dataset)
+    y.d <- y[, , dataset, setting]
+    obs <- rep(c(T, F), 100)[1:ns]
+    y.o <- y.d[obs, ]
+    x.o <- x[obs, , ]
+    s.o <- s[obs, ]
+
+    # y.validate[, , d] <- y.d[!obs, ]
+    x.p <- x[!obs, , ]
+    s.p <- s[!obs, ]
+
+    cat("  start: skew t-5 (T=0.90) - Set", dataset, "\n")
+    tic <- proc.time()
+    fit.1[[d]] <- tryCatch(
+                       mcmc(y=y.o, s=s.o, x=x.o, s.pred=s.p, x.pred=x.p,
+                       method="t", skew=T, thresh=0.90, nknots=5,
+                       iterplot=F, iters=iters, burn=burn, 
+                       update=update, thin=thin,
+                       nu.init=0.5, cov.model="exponential", rho.prior="cont"),
+                       error = function(e) {
+                         tryCatch(mcmc(y=y.o, s=s.o, x=x.o, s.pred=s.p, x.pred=x.p,
+                         method="t", skew=T, thresh=0.90, nknots=5,
+                         iterplot=F, iters=iters, burn=burn, 
+                         update=update, thin=thin,
+                         nu.init=0.5, cov.model="exponential", rho.prior="disc"),
+                         error = function(e) {
+                           cat("dataset", d, "not working \n")
+                           "no results"
+                         })
+                       })
+
+    toc <- proc.time()
+    cat("  skew t-5 (T=0.90) took:", (toc - tic)[3], "\n")
+    cat("  end: skew t-5 (T=0.90) \n")
+    cat("------------------\n")
+
+    save(fit.1, file=outputfile)
+  # }
+  
+  rm(fit.1)
+  gc()
+# }
+
+# for (g in 1) {
+g <- 5
+  # fit.1 <- vector(mode="list", length=nsets)
+  # y.validate <- array(NA, dim=c(ntest, nt, nsets))
+  outputfile <- paste(setting, "-", analysis, "-", g, ".RData", sep="")
+  load(outputfile)
+  
+  start <- proc.time()
+  # for (d in 1:nsets) {
+  d <- 4
+    dataset <- (g-1) * 5 + d
+    cat("start dataset", dataset, "\n")
+    set.seed(setting * 100 + dataset)
+    y.d <- y[, , dataset, setting]
+    obs <- rep(c(T, F), 100)[1:ns]
+    y.o <- y.d[obs, ]
+    x.o <- x[obs, , ]
+    s.o <- s[obs, ]
+
+    # y.validate[, , d] <- y.d[!obs, ]
+    x.p <- x[!obs, , ]
+    s.p <- s[!obs, ]
+
+    cat("  start: skew t-5 (T=0.90) - Set", dataset, "\n")
+    tic <- proc.time()
+    fit.1[[d]] <- tryCatch(
+                       mcmc(y=y.o, s=s.o, x=x.o, s.pred=s.p, x.pred=x.p,
+                       method="t", skew=T, thresh=0.90, nknots=5,
+                       iterplot=F, iters=iters, burn=burn, 
+                       update=update, thin=thin,
+                       nu.init=0.5, cov.model="exponential", rho.prior="cont"),
+                       error = function(e) {
+                         tryCatch(mcmc(y=y.o, s=s.o, x=x.o, s.pred=s.p, x.pred=x.p,
+                         method="t", skew=T, thresh=0.90, nknots=5,
+                         iterplot=F, iters=iters, burn=burn, 
+                         update=update, thin=thin,
+                         nu.init=0.5, cov.model="exponential", rho.prior="disc"),
+                         error = function(e) {
+                           cat("dataset", d, "not working \n")
+                           "no results"
+                         })
+                       })
+
+    toc <- proc.time()
+    cat("  skew t-5 (T=0.90) took:", (toc - tic)[3], "\n")
+    cat("  end: skew t-5 (T=0.90) \n")
+    cat("------------------\n")
+
+    save(fit.1, file=outputfile)
+  # }
+  
+  rm(fit.1)
+  gc()
+# }
