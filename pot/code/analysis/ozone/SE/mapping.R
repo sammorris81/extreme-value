b4fab2fbcc67056f27bacda5645f2ac61e5ffee2
samorris <samorris@sam-macbookair.local>
2014-08-04 08:18:14 -0400

    extract more info

--- pot/code/analysis/ozone/SE/mapping.R
+++ pot/code/analysis/ozone/SE/mapping.R
@@ -25,97 +25,97 @@ rm(list=ls())
   # X.preds[, t, 3] <- s.scale.preds[, 2]
 # }
 
-probs <- c(0.50, 0.90, 0.95, 0.99)
-quantiles.90 <- quantiles.95 <- quantiles.99 <- matrix(NA, nrow=439, ncol=5)
-p.exceed.75 <- array(NA, dim=c(439, 92, 5))
-post.med <- array(NA, dim=c(439, 92, 5))
+# # probs <- c(0.50, 0.90, 0.95, 0.99)
+# quantiles.90 <- quantiles.95 <- quantiles.99 <- matrix(NA, nrow=439, ncol=5)
+# p.exceed.75 <- array(NA, dim=c(439, 92, 5))
+# post.med <- array(NA, dim=c(439, 92, 5))
 
-tau <- matrix(NA, 92, 3)
-beta <- matrix(NA, 3, 3)
-rho <- rep(NA, 3)
-nu <- rep(NA, 3)
-alpha <- rep(NA, 3)
-
-
-load('./OzoneFull1.RData')
-tau[, 1] <- apply(fit.1$tau, 2, mean)
-beta[, 1] <- apply(fit.1$beta, 2, mean)
-rho[1] <- mean(fit.1$rho)
-nu[1] <- mean(fit.1$nu)
-alpha[1] <- mean(fit.1$alpha)
-yp <- fit.1$yp
-post.med[, , 1] <- apply(yp, c(2, 3), quantile, probs=0.50)
-quantiles.90[, 1] <- apply(yp, 2, quantile, probs=0.90)
-quantiles.95[, 1] <- apply(yp, 2, quantile, probs=0.95)
-quantiles.99[, 1] <- apply(yp, 2, quantile, probs=0.99)
-for (i in 1:439) {
-  for (t in 1:92) {
-    p.exceed.75[i, t, 1] <- mean(yp[, i, t] > 75)
-  }
-}
+# tau <- matrix(NA, 92, 3)
+# beta <- matrix(NA, 3, 3)
+# rho <- rep(NA, 3)
+# nu <- rep(NA, 3)
+# alpha <- rep(NA, 3)
 
 
-load('./OzoneFull2.RData')
-tau[, 2] <- apply(fit.2$tau, 2, mean)
-beta[, 2] <- apply(fit.2$beta, 2, mean)
-rho[2] <- mean(fit.2$rho)
-nu[2] <- mean(fit.2$nu)
-alpha[2] <- mean(fit.2$alpha)
-yp <- fit.2$yp
-post.med[, , 2] <- apply(yp, 2, quantile, probs=0.50)
-quantiles.90[, 2] <- apply(yp, 2, quantile, probs=0.90)
-quantiles.95[, 2] <- apply(yp, 2, quantile, probs=0.95)
-quantiles.99[, 2] <- apply(yp, 2, quantile, probs=0.99)
-for (i in 1:439) {
-  for (t in 1:92) {
-    p.exceed.75[i, t, 2] <- mean(yp[, i, t] > 75)
-  }
-}
+# load('./OzoneFull1.RData')
+# tau[, 1] <- apply(fit.1$tau, 2, mean)
+# beta[, 1] <- apply(fit.1$beta, 2, mean)
+# rho[1] <- mean(fit.1$rho)
+# nu[1] <- mean(fit.1$nu)
+# alpha[1] <- mean(fit.1$alpha)
+# yp <- fit.1$yp
+# post.med[, , 1] <- apply(yp, c(2, 3), quantile, probs=0.50)
+# quantiles.90[, 1] <- apply(yp, 2, quantile, probs=0.90)
+# quantiles.95[, 1] <- apply(yp, 2, quantile, probs=0.95)
+# quantiles.99[, 1] <- apply(yp, 2, quantile, probs=0.99)
+# for (i in 1:439) {
+  # for (t in 1:92) {
+    # p.exceed.75[i, t, 1] <- mean(yp[, i, t] > 75)
+  # }
+# }
 
-load('./OzoneFull3.RData')
-tau[, 3] <- apply(fit.3$tau, 2, mean)
-beta[, 3] <- apply(fit.3$beta, 2, mean)
-rho[3] <- mean(fit.3$rho)
-nu[3] <- mean(fit.3$nu)
-alpha[3] <- mean(fit.3$alpha)
-z.alpha <- mean(fit.3$z.alpha)
-z <- apply(fit.3$z, 2, mean)
-yp <- fit.3$yp
-post.med[, , 3] <- apply(yp, c(2, 3), quantile, probs=0.50)
-quantiles.90[, 3] <- apply(yp, 2, quantile, probs=0.90)
-quantiles.95[, 3] <- apply(yp, 2, quantile, probs=0.95)
-quantiles.99[, 3] <- apply(yp, 2, quantile, probs=0.99)
-for (i in 1:439) {
-  for (t in 1:92) {
-    p.exceed.75[i, t, 3] <- mean(yp[, i, t] > 75)
-  }
-}
 
-load('./OzoneFull4.RData')
-yp <- fit.4$yp
-post.med[, , 4] <- apply(yp, c(2, 3), quantile, probs=0.50)
-quantiles.90[, 4] <- apply(yp, 2, quantile, probs=0.90)
-quantiles.95[, 4] <- apply(yp, 2, quantile, probs=0.95)
-quantiles.99[, 4] <- apply(yp, 2, quantile, probs=0.99)
-for (i in 1:439) {
-  for (t in 1:92) {
-    p.exceed.75[i, t, 4] <- mean(yp[, i, t] > 75)
-  }
-}
+# load('./OzoneFull2.RData')
+# tau[, 2] <- apply(fit.2$tau, 2, mean)
+# beta[, 2] <- apply(fit.2$beta, 2, mean)
+# rho[2] <- mean(fit.2$rho)
+# nu[2] <- mean(fit.2$nu)
+# alpha[2] <- mean(fit.2$alpha)
+# yp <- fit.2$yp
+# post.med[, , 2] <- apply(yp, 2, quantile, probs=0.50)
+# quantiles.90[, 2] <- apply(yp, 2, quantile, probs=0.90)
+# quantiles.95[, 2] <- apply(yp, 2, quantile, probs=0.95)
+# quantiles.99[, 2] <- apply(yp, 2, quantile, probs=0.99)
+# for (i in 1:439) {
+  # for (t in 1:92) {
+    # p.exceed.75[i, t, 2] <- mean(yp[, i, t] > 75)
+  # }
+# }
 
-load('./OzoneFull5.RData')
-yp <- fit.5$yp
-post.med[, , 5] <- apply(yp, c(2, 3), quantile, probs=0.50)
-quantiles.90[, 5] <- apply(yp, 2, quantile, probs=0.90)
-quantiles.95[, 5] <- apply(yp, 2, quantile, probs=0.95)
-quantiles.99[, 5] <- apply(yp, 2, quantile, probs=0.99)
-for (i in 1:439) {
-  for (t in 1:92) {
-    p.exceed.75[i, t, 5] <- mean(yp[, i, t] > 75)
-  }
-}
+# load('./OzoneFull3.RData')
+# tau[, 3] <- apply(fit.3$tau, 2, mean)
+# beta[, 3] <- apply(fit.3$beta, 2, mean)
+# rho[3] <- mean(fit.3$rho)
+# nu[3] <- mean(fit.3$nu)
+# alpha[3] <- mean(fit.3$alpha)
+# z.alpha <- mean(fit.3$z.alpha)
+# z <- apply(fit.3$z, 2, mean)
+# yp <- fit.3$yp
+# post.med[, , 3] <- apply(yp, c(2, 3), quantile, probs=0.50)
+# quantiles.90[, 3] <- apply(yp, 2, quantile, probs=0.90)
+# quantiles.95[, 3] <- apply(yp, 2, quantile, probs=0.95)
+# quantiles.99[, 3] <- apply(yp, 2, quantile, probs=0.99)
+# for (i in 1:439) {
+  # for (t in 1:92) {
+    # p.exceed.75[i, t, 3] <- mean(yp[, i, t] > 75)
+  # }
+# }
 
-save(post.med, quantiles.90, quantiles.95, quantiles.99, p.exceed.75, tau, beta, rho, nu, alpha, z.alpha, z, file="predictions.RData")
+# load('./OzoneFull4.RData')
+# yp <- fit.4$yp
+# post.med[, , 4] <- apply(yp, c(2, 3), quantile, probs=0.50)
+# quantiles.90[, 4] <- apply(yp, 2, quantile, probs=0.90)
+# quantiles.95[, 4] <- apply(yp, 2, quantile, probs=0.95)
+# quantiles.99[, 4] <- apply(yp, 2, quantile, probs=0.99)
+# for (i in 1:439) {
+  # for (t in 1:92) {
+    # p.exceed.75[i, t, 4] <- mean(yp[, i, t] > 75)
+  # }
+# }
+
+# load('./OzoneFull5.RData')
+# yp <- fit.5$yp
+# post.med[, , 5] <- apply(yp, c(2, 3), quantile, probs=0.50)
+# quantiles.90[, 5] <- apply(yp, 2, quantile, probs=0.90)
+# quantiles.95[, 5] <- apply(yp, 2, quantile, probs=0.95)
+# quantiles.99[, 5] <- apply(yp, 2, quantile, probs=0.99)
+# for (i in 1:439) {
+  # for (t in 1:92) {
+    # p.exceed.75[i, t, 5] <- mean(yp[, i, t] > 75)
+  # }
+# }
+
+# save(post.med, quantiles.90, quantiles.95, quantiles.99, p.exceed.75, tau, beta, rho, nu, alpha, z.alpha, z, file="predictions.RData")
 
 # # plot monitoring station ozone locations
 # plot(s, type="p", xlab="", ylab="")
@@ -228,16 +228,64 @@ save(post.med, quantiles.90, quantiles.95, quantiles.99, p.exceed.75, tau, beta,
 # lines(l)
 
 # what if I took the posterior mean for each of the model parameters (gaussian and t-1 T=0.9 and skew-t-1 T=0.90 and just generated the data)
-# s1.preds <- seq(1050, 1800, length=50)
-# s2.preds <- seq(-860, -250, length=50)
-# s.preds <- expand.grid(s1.preds, s2.preds)
+load("predictions.RData")
+load('cv-setup-se.RData')
 
-# X.preds <- array(1, dim=c(nrow(s.preds), nt, 3))
-# for (t in 1:nt) {
-  # X.preds[, t, 2] <- s.scale.preds[, 1]
-  # X.preds[, t, 3] <- s.scale.preds[, 2]
-# }
+nt <- 92
+s1.preds <- seq(1050, 1800, length=30)
+s2.preds <- seq(-860, -250, length=30)
+s.preds <- expand.grid(s1.preds, s2.preds)
+s.scale.preds <- matrix(NA, nrow=nrow(s.preds), ncol=ncol(s.preds))
+s.scale.preds[,1] <- (s.preds[,1] - range(s[,1])[1])/(range(s[,1])[2] - range(s[,1])[1])
+s.scale.preds[,2] <- (s.preds[,2] - range(s[,2])[1])/(range(s[,2])[2] - range(s[,2])[1])
+np <- nrow(s.preds)
 
-# d <- rdist(s.preds)
-# diag(d) <- 0
-# cor <- matern()
+X.preds <- array(1, dim=c(nrow(s.preds), nt, 3))
+for (t in 1:nt) {
+  X.preds[, t, 2] <- s.scale.preds[, 1]
+  X.preds[, t, 3] <- s.scale.preds[, 2]
+}
+
+d <- rdist(s.preds)
+diag(d) <- 0
+
+# gaussian
+cor <- alpha[1] * matern(d, rho[1], nu[1])
+diag(cor) <- 1
+y.pred.gau <- array(NA, dim=c(1000, np, nt))
+for (t in 1:nt) {
+  mean.t <- X.preds[, t, ] %*% beta[, 1]
+  cov.t <- cor / tau[t, 1]
+  chol.t <- chol(cov.t)
+  for(i in 1:1000) {
+    y.pred.gau[i, , t] <- mean.t + chol.t %*% rnorm(np, 0, 1)
+  }
+}
+
+# t-1 (T=0.90)
+cor <- alpha[2] * matern(d, rho[2], nu[2])
+diag(cor) <- 1
+y.pred.t1 <- array(NA, dim=c(1000, np, nt))
+for (t in 1:nt) {
+  mean.t <- X.preds[, t, ] %*% beta[, 2]
+  cov.t <- cor / tau[t, 1]
+  chol.t <- chol(cov.t)
+  for(i in 1:1000) {
+    y.pred.t1[i, , t] <- mean.t + chol.t %*% rnorm(np, 0, 1)
+  }
+}
+
+# skew t-1 (T=0.90)
+cor <- alpha[3] * matern(d, rho[3], nu[3])
+diag(cor) <- 1
+y.pred.st1 <- array(NA, dim=c(1000, np, nt))
+for (t in 1:nt) {
+  mean.t <- X.preds[, t, ] %*% beta[, 3] + z.alpha * z[t]
+  cov.t <- cor / tau[t, 1]
+  chol.t <- chol(cov.t)
+  for(i in 1:1000) {
+    y.pred.t1T[i, , t] <- mean.t + chol.t %*% rnorm(np, 0, 1)
+  }
+}
+
+save(y.pred.gau, y.pred.t1, y.pred.t1T, filename="map.RData")
