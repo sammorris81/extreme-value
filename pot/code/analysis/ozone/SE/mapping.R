ecab837d9bf130964f4c43edde28aa37dee0e791
samorris <samorris@sam-macbookair.local>
2014-08-03 21:53:15 -0400

    extract different info

--- pot/code/analysis/ozone/SE/mapping.R
+++ pot/code/analysis/ozone/SE/mapping.R
@@ -6,51 +6,57 @@ p.exceed.75 <- array(NA, dim=c(439, 92, 5))
 
 load('./OzoneFull1.RData')
 yp <- fit.1$yp
-for (i in 1:3) {
-  quantiles[1, , i] <- apply(yp, 2, quantile, probs=probs[i])
-}
-# for each site, how much of the posterior predictive is above 75ppb
+quantiles.90 <- apply(yp, c(2, 3), quantile, probs=0.90)
+quantiles.95 <- apply(yp, c(2, 3), quantile, probs=0.95)
+quantiles.99 <- apply(yp, c(2, 3), quantile, probs=0.99)
 for (i in 1:439) {
-  p.exceed.75[1, i] <- mean(yp[, i, ] > 75)
+  for (t in 1:92) {
+    p.exceed.75[i, t, 1] <- mean(yp[, i, t] > 75)
+  }
 }
+
 load('./OzoneFull2.RData')
 yp <- fit.2$yp
-for (i in 1:3) {
-  quantiles[2, , i] <- apply(yp, 2, quantile, probs=probs[i])
-}
-# for each site, how much of the posterior predictive is above 75ppb
+quantiles.90 <- apply(yp, c(2, 3), quantile, probs=0.90)
+quantiles.95 <- apply(yp, c(2, 3), quantile, probs=0.95)
+quantiles.99 <- apply(yp, c(2, 3), quantile, probs=0.99)
 for (i in 1:439) {
-  p.exceed.75[2, i] <- mean(yp[, i, ] > 75)
+  for (t in 1:92) {
+    p.exceed.75[i, t, 2] <- mean(yp[, i, t] > 75)
+  }
 }
 
 load('./OzoneFull3.RData')
 yp <- fit.3$yp
-for (i in 1:3) {
-  quantiles[3, , i] <- apply(yp, 2, quantile, probs=probs[i])
-}
-# for each site, how much of the posterior predictive is above 75ppb
+quantiles.90 <- apply(yp, c(2, 3), quantile, probs=0.90)
+quantiles.95 <- apply(yp, c(2, 3), quantile, probs=0.95)
+quantiles.99 <- apply(yp, c(2, 3), quantile, probs=0.99)
 for (i in 1:439) {
-  p.exceed.75[3, i] <- mean(yp[, i, ] > 75)
+  for (t in 1:92) {
+    p.exceed.75[i, t, 3] <- mean(yp[, i, t] > 75)
+  }
 }
 
 load('./OzoneFull4.RData')
 yp <- fit.4$yp
-for (i in 1:3) {
-  quantiles[4, , i] <- apply(yp, 2, quantile, probs=probs[i])
-}
-# for each site, how much of the posterior predictive is above 75ppb
+quantiles.90 <- apply(yp, c(2, 3), quantile, probs=0.90)
+quantiles.95 <- apply(yp, c(2, 3), quantile, probs=0.95)
+quantiles.99 <- apply(yp, c(2, 3), quantile, probs=0.99)
 for (i in 1:439) {
-  p.exceed.75[4, i] <- mean(yp[, i, ] > 75)
+  for (t in 1:92) {
+    p.exceed.75[i, t, 4] <- mean(yp[, i, t] > 75)
+  }
 }
 
 load('./OzoneFull5.RData')
 yp <- fit.5$yp
-for (i in 1:3) {
-  quantiles[5, , i] <- apply(yp, 2, quantile, probs=probs[i])
-}
-# for each site, how much of the posterior predictive is above 75ppb
+quantiles.90 <- apply(yp, c(2, 3), quantile, probs=0.90)
+quantiles.95 <- apply(yp, c(2, 3), quantile, probs=0.95)
+quantiles.99 <- apply(yp, c(2, 3), quantile, probs=0.99)
 for (i in 1:439) {
-  p.exceed.75[5, i] <- mean(yp[, i, ] > 75)
+  for (t in 1:92) {
+    p.exceed.75[i, t, 5] <- mean(yp[, i, t] > 75)
+  }
 }
 
 save(quantiles, p.exceed.75, file="predictions.RData")
