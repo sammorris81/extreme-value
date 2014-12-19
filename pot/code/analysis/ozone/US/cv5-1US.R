fffce3a5d09b472ab6b14de09dddd36afedda9b3
samorris <samorris@sam-macbookair.local>
2014-12-18 23:38:47 -0600

    parameter name change to gamma

--- pot/code/analysis/ozone/US/cv5-1US.R
+++ pot/code/analysis/ozone/US/cv5-1US.R
@@ -41,7 +41,7 @@ for(val in 1:2){
 	                   thresh.all=threshold, thresh.quant=thresh.quant, nknots=nknots, 
                        iters=30000, burn=25000, update=500, iterplot=F,
                        beta.init=beta.init, tau.init=tau.init, rho.init=1,
-                       nu.init=0.5, alpha.init=0.5)
+                       nu.init=0.5, gamma.init=0.5)
 	toc.set <- proc.time()
 	time.set <- (toc.set - tic.set)[3]
 	
