18f1a2d2c8bd433b58b83cfb43f30caa3fc00035
sam-mba <sam.morris81@gmail.com>
2015-01-03 00:29:15 -0600

    get full map of US for gaussian

--- pot/code/analysis/ozone/US-all/us-all-full-1.R
+++ pot/code/analysis/ozone/US-all/us-all-full-1.R
@@ -1,11 +1,11 @@
 options(warn=2)
 library(fields)
-library(geoR)
+library(SpatialTools)
 library(mvtnorm)
 
 rm(list=ls())
 load('us-all-setup.RData')
-source('../../../R/mcmc.R')
+source('../../../R/mcmc.R', chdir=T)
 source('../../../R/auxfunctions.R')
 
 setting <- 1
@@ -19,35 +19,46 @@ outputfile <- paste("OzoneFull-", setting, "US.RData", sep="")
 
 start <- proc.time()
 
-val <- 1
+set.seed(4218)
+total <- 400
+NE <- S[, 1] > 0 & S[, 2] > 0
+NW <- S[, 1] < 0 & S[, 2] > 0
+SE <- S[, 1] > 0 & S[, 2] < 0
+SW <- S[, 1] < 0 & S[, 2] < 0
+nNE <- round(mean(NE) * total)
+nNW <- round(mean(NW) * total)
+nSE <- round(mean(SE) * total)
+nSW <- round(mean(SW) * total)
+observed <- c(sample(which(NE), nNE), sample(which(NW), nNW),
+                sample(which(SE), nSE), sample(which(SW), nSW))
 
-set.seed(setting*100 + val)
+set.seed(setting*100)
 
-cat("CV", val, "started \n")
-val.idx <- cv.lst[[val]]
-y.o <- Y[-val.idx,]
-X.o <- X[-val.idx, , ]
-S.o <- S[-val.idx,]
+y.o <- Y[observed, ]
+X.o <- X[observed, , ]
+S.o <- S[observed, ]
 
 # select every third CMAQ value
 S.p <- expand.grid(x, y)
-keep.these <- which((S.p[, 1] > 0) & (S.p[, 1] < 2.35) & S.p[, 2] > -1.6 & S.p[, 2] < 1.2)
+keep.these <- which((S.p[, 1] > -2.3) & (S.p[, 1] < 2.4) & S.p[, 2] > -1.6 & S.p[, 2] < 1.3)
 CMAQ.p <- CMAQ[keep.these, ]
 S.p    <- S.p[keep.these, ]
+nx <- length(unique(S.p[, 1]))
+ny <- length(unique(S.p[, 2]))
 
 # # check the plot against the full
 # zlim=c(0, 122)
-# quilt.plot(x=S.p[, 1], y=S.p[, 2], matrix(CMAQ.p[, 5]), zlim=zlim)
+# quilt.plot(x=S.p[, 1], y=S.p[, 2], matrix(CMAQ.p[, 5]), zlim=zlim, nx=nx, ny=ny)
 # lines(borders/1000)
 
 #### Thin the rows and columns by 3
 # First, figure out what the x and y values are for the rows
 # and columns we should keep
 unique.x <- unique(S.p[, 1])
-keep.x <- unique.x[seq(1, length(unique.x), by=3)]
+keep.x <- unique.x[seq(1, length(unique.x), by=4)]
 nx <- length(keep.x)
 unique.y <- unique(S.p[, 2])
-keep.y <- unique.y[seq(1, length(unique(S.p[, 2])), by=3)]
+keep.y <- unique.y[seq(1, length(unique(S.p[, 2])), by=4)]
 ny <- length(keep.y)
 keep.these <- which((S.p[, 1] %in% keep.x) & (S.p[, 2] %in% keep.y))
 
@@ -56,12 +67,13 @@ CMAQ.p <- CMAQ.p[keep.these, ]
 S.p    <- S.p[keep.these, ]
 
 # # check the plot against the full
+# dev.new()
 # zlim=c(0, 122)
 # quilt.plot(x=S.p[, 1], y=S.p[, 2], matrix(CMAQ.p[, 5]), zlim=zlim, nx=nx, ny=ny)
 # lines(borders/1000)
 
 # reshape CMAQ to have the correct form for the mcmc function
-X.p <- array(1, dim=nrow(CMAQ.p), nt, 2)
+X.p <- array(1, dim=c(nrow(CMAQ.p), nt, 2))
 for (t in 1:nt) {
  X.p[, t, 2] <- CMAQ.p[, t]
 }
