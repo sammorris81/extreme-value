0a5ec32e504d3c338bd0edce13d57c391f120cbb
sam-ubuntu <samorris@ncsu.edu>
2015-04-06 10:45:47 -0400

    updated plots for manuscript

--- code/analysis/ozone/US-all/us-all-results.R
+++ code/analysis/ozone/US-all/us-all-results.R
@@ -863,12 +863,12 @@ lines(borders/1000)
 
 quilt.plot(x=S.p[, 1], y=S.p[, 2], matrix(set.3.99), nx=nx, ny=ny,
            yaxt="n", xaxt="n", zlim=c(60, 120),
-           main="(b) Skew-t, K=1, No Time Series, T=0", cex=1.5)
+           main="(b) Skew-t, K=1, T=0, No Time Series, ", cex=1.5)
 lines(borders/1000)
 
 quilt.plot(x=S.p[, 1], y=S.p[, 2], matrix(set.8.99), nx=nx, ny=ny,
            yaxt="n", xaxt="n",zlim=c(60, 120),
-           main="(c) Skew-t, K=5, No Time Series, T=50")
+           main="(c) Skew-t, K=5, T=50, No Time Series")
 lines(borders/1000)
 #
 # quilt.plot(x=S.p[, 1], y=S.p[, 2], matrix(set.59.99), nx=nx, ny=ny,
@@ -878,7 +878,7 @@ lines(borders/1000)
 
 quilt.plot(x=S.p[, 1], y=S.p[, 2], matrix(set.71.99), nx=nx, ny=ny,
            yaxt="n", xaxt="n", zlim=c(60, 120),
-           main="(d) Sym-t, K=10, Time Series, T=75", cex=1.5)
+           main="(d) Sym-t, K=10, T=75, Time Series", cex=1.5)
 lines(borders/1000)
 
 diff.71.1 = set.71.99 - set.1.99
