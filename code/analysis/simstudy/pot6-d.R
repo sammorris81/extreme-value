1ca346645c5e312a9d8ce9344383babb708fb61d
sam-ubuntu <samorris@ncsu.edu>
2015-05-04 20:38:40 -0400

    beowulf has already completed the first set of 5 datasets

--- code/analysis/simstudy/pot6-d.R
+++ code/analysis/simstudy/pot6-d.R
@@ -45,7 +45,7 @@ analysis <- "d"
 iters <- 20000; burn <- 10000; update <- 1000; thin <- 1
 nsets <- 5
 
-for (g in 1:10) {
+for (g in 2:10) {
   fit.1 <- vector(mode="list", length=nsets)
   y.validate <- array(NA, dim=c(ntest, nt, nsets))
   outputfile <- paste(setting, "-", analysis, "-", g, ".RData", sep="")
