ef5a1c8861dd3fe7ca44633197f07399faf7925d
sam-mba <sam.morris81@gmail.com>
2015-05-08 13:50:54 -0400

    Adding in tryCatch for log(canA)

--- code/analysis/simstudy/max-stab/MCMC4MaxStable.R
+++ code/analysis/simstudy/max-stab/MCMC4MaxStable.R
@@ -145,22 +145,24 @@ maxstable<-function(y, x, s, thresh, knots, sp = NULL, xp = NULL,
           l2     <- get.level.sca(a[t, k], cuts)
           WWW    <- FAC.star[, k]
           canA   <- A[t, ] + WWW * (cana - a[t, k])
-          if (sum(canA > 0) > 0) {  # numerical stability
-            cc     <- -canA * parts$expo + W * log(canA)
-            # canlp  <- dPS(cana, alpha)
-            canlp  <- dPS_cpp_sca(cana, alpha, psi, BinWidth, threads)
-            loga   <- log(a[t, k])
-            logcana <- log(cana)
-            R <- sum(cc - ccc) + canlp - curlp[t, k] +
-                 dlognormal(loga, logcana, MHa[l2]) -
-                 dlognormal(logcana, loga, MHa[l1])
-            if (!is.na(exp(R))) { if (runif(1) < exp(R)) {
-                a[t, k]     <- cana
-                A[t, ]      <- canA
-                ccc         <- cc
-                curlp[t, k] <- canlp
-            } }
-          }
+          cc     <- -canA * parts$expo + W *
+                    tryCatch(log(canA), error=function(e) {
+                      print(paste("Died on cc for k=", k, ", t=", t, sep=""))
+                      save(a, A, canA, WWW, cana, file="logcanA.RData")
+                    })
+          # canlp  <- dPS(cana, alpha)
+          canlp  <- dPS_cpp_sca(cana, alpha, psi, BinWidth, threads)
+          loga   <- log(a[t, k])
+          logcana <- log(cana)
+          R <- sum(cc - ccc) + canlp - curlp[t, k] +
+               dlognormal(loga, logcana, MHa[l2]) -
+               dlognormal(logcana, loga, MHa[l1])
+          if (!is.na(exp(R))) { if (runif(1) < exp(R)) {
+              a[t, k]     <- cana
+              A[t, ]      <- canA
+              ccc         <- cc
+              curlp[t, k] <- canlp
+          } }
         }
 
         curll[t, ] <- loglike(y[t, ], A[t, ], B[t, , ], thresh)
