6e50f43678b9a8d9a640ab3442dce6a701ff05ca
Sam Morris <sam.morris81@gmail.com>
2016-05-16 13:28:59 -0500

    edits to the manuscript for revision

--- markdown/circle-intersect.R
+++ markdown/circle-intersect.R
@@ -101,14 +101,64 @@ yplot2.neg <- yplot.neg * d2 + s2[2]
 ylims <- range(c(yplot.pos, yplot1.pos, yplot2.pos, yplot.neg, yplot1.neg, yplot2.neg))
 xlims <- range(c(xplot, xplot1, xplot2))
 
-plot(0, 0, ylim = ylims, xlim = xlims, type = "n")
+quartz(width = 10, height = 5)
+par(mar = c(0.2, 0.2, 0.2, 0.2))
+plot(0, 0, ylim = ylims, xlim = xlims, type = "n", 
+     xaxt = "n", xlab = "", yaxt = "n", ylab = "",
+     cex = 1.5)
+curve(sqrt(1 - x^2), from = -1, to = 1, add = TRUE, lty = 1, lwd = 1.5)
+curve(-sqrt(1 - x^2), from = -1, to = 1, add = TRUE, lty = 1, lwd = 1.5)
+curve(sqrt(1 - (x + 1)^2), from = -2, to = -0.5, add = TRUE, lty = 3, lwd = 1.5)
+curve(sqrt(1 - (x + 1)^2), from = -0.5, to = 0, add = TRUE, lty = 1, lwd = 1.5)
+curve(-sqrt(1 - (x + 1)^2), from = -2, to = -0.5, add = TRUE, lty = 3, lwd = 1.5)
+curve(-sqrt(1 - (x + 1)^2), from = -0.5, to = 0, add = TRUE, lty = 1, lwd = 1.5)
+curve(sqrt(1 - (x - 1)^2), from = 0.5, to = 2, add = TRUE, lty = 3, lwd = 1.5)
+curve(sqrt(1 - (x - 1)^2), from = 0, to = 0.5, add = TRUE, lty = 1, lwd = 1.5)
+curve(-sqrt(1 - (x - 1)^2), from = 0.5, to = 2, add = TRUE, lty = 3, lwd = 1.5)
+curve(-sqrt(1 - (x - 1)^2), from = 0, to = 0.5, add = TRUE, lty = 1, lwd = 1.5)
+
+poly.x1 <- xplot[xplot <= 0]
+poly.y1h <- pmin(sqrt(1 - poly.x1^2), sqrt(1 - (poly.x1 + 1)^2))
+poly.y1l <- pmax(-sqrt(1 - poly.x1^2), -sqrt(1 - (poly.x1 + 1)^2))
+polygon(c(poly.x1, rev(poly.x1)), c(poly.y1l, rev(poly.y1h)),
+        col = "dodgerblue1")
+
+poly.x1 <- xplot[xplot >= 0]
+poly.y1h <- pmin(sqrt(1 - poly.x1^2), sqrt(1 - (poly.x1 - 1)^2))
+poly.y1l <- pmax(-sqrt(1 - poly.x1^2), -sqrt(1 - (poly.x1 - 1)^2))
+polygon(c(poly.x1, rev(poly.x1)), c(poly.y1l, rev(poly.y1h)),
+        col = "dodgerblue1")
+
+poly.x1 <- xplot[xplot >= -0.5 & xplot <= 0.5]
+poly.y1h <- sqrt(1 - poly.x1^2)
+poly.y1l <- c(sqrt(1 - (xplot[xplot <= 0 & xplot >= -0.5] + 1)^2),
+              sqrt(1 - (xplot[xplot <= 0.5 & xplot >= 0] - 1)^2))
+polygon(c(poly.x1, rev(poly.x1)), c(poly.y1l, rev(poly.y1h)),
+        col = "firebrick1")
+
+poly.x1 <- xplot[xplot >= -0.5 & xplot <= 0.5]
+poly.y1h <- c(-sqrt(1 - (xplot[xplot <= 0 & xplot >= -0.5] + 1)^2),
+              -sqrt(1 - (xplot[xplot <= 0.5 & xplot >= 0] - 1)^2))
+poly.y1l <- -sqrt(1 - poly.x1^2)
+polygon(c(poly.x1, rev(poly.x1)), c(poly.y1l, rev(poly.y1h)),
+        col = "firebrick1")
+
+# points(0, 0, pch = 19, cex = 1.5)
+# text(x = 0.1, y = 0.07, adj = 0.5, bquote(v[1]), cex = 1.5)
+
+points(1, 0, pch = 19, cex = 1.5)
+text(x = 1.1, y = 0.07, adj = 0.5, bquote(s[1]), cex = 1.5)
+
+points(-1, 0, pch = 19, cex = 1.5)
+text(x = -1.1, y = 0.07, adj = 0.5, bquote(s[2]), cex = 1.5)
+
+text(x = 0.75 + 0.12, y = (sqrt(1 - 0.75^2)) + 0.07, bquote(italic(A)), cex = 1.5)
+text(x = 0, y = 0.75, adj = 0.5, bquote(italic(R)[1]), cex = 1.5)
+text(x = 0, y = -0.75, adj = 0.5, bquote(italic(R)[2]), cex = 1.5)
+text(x = -0.5, y = 0, adj = 0.5, bquote(italic(B)[1]), cex = 1.5)
+text(x = 0.5, y = 0, adj = 0.5, bquote(italic(B)[2]), cex = 1.5)
+
 abline(h = 0)
-lines(xplot, yplot.pos)
-lines(xplot, yplot.neg)
-lines(xplot1, yplot1.pos)
-lines(xplot1, yplot1.neg)
-lines(xplot2, yplot2.pos)
-lines(xplot2, yplot2.neg)
 
 reps <- 100000
 area <- 0
