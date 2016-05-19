fdb2063e9be18a427a8f0287c6c3d52856d4f461
Sam Morris <sam.morris81@gmail.com>
2016-05-18 23:14:47 -0500

    rerunning simulations for skewed methods - for revision

---
+++ code/analysis/simstudy/launch-2-c.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# CWD=`pwd`
+RBIN=$HOME/packages/R/lib64/R/bin/R
+SIM=$HOME/repos-git/spatial-skew-t/code/analysis/simstudy
+
+$RBIN CMD BATCH --vanilla --no-save $SIM/pot2-c.R $SIM/pot2-c.out 2>&1
+exit 0
