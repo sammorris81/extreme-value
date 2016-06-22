3f78e299f5cc46ad540fcbb02b81e15e057cbf6d
Sam Morris <smorris@uwlax.edu>
2016-06-22 16:58:39 -0500

    add in some print statements

--- code/analysis/ozone/US-all/make-map-3.R
+++ code/analysis/ozone/US-all/make-map-3.R
@@ -21,13 +21,18 @@ load('us-all-pred-3.RData')
 yp <- y.pred
 np <- dim(yp)[2]
 niters <- dim(yp)[1]
+
+# get the posterior distribution of the 95th and 99th quantile for each site
+print("getting q95")
 set.3.95.q <- apply(yp, c(1, 2), quantile, probs = 0.95)
+print("getting q99")
 set.3.95.q <- apply(yp, c(1, 2), quantile, probs = 0.99)
 # set.3.95 <- apply(yp, c(2), quantile, probs=0.95)
 # set.3.99 <- apply(yp, c(2), quantile, probs=0.99)
 set.3.95 <- apply(set.3.95.q, 2, mean)
 set.3.99 <- apply(set.3.99.q, 2, mean)
 
+print("getting probs of exceedance")
 set.3.p.below <- matrix(0, np, nt)
 for (i in 1:np) { for (t in 1:nt) {
   set.3.p.below[i, t] <- mean(yp[, i, t] <= threshold)
--- code/analysis/ozone/US-all/make-map-71.R
+++ code/analysis/ozone/US-all/make-map-71.R
@@ -21,13 +21,18 @@ load('us-all-pred-71.RData')
 yp <- y.pred
 np <- dim(yp)[2]
 niters <- dim(yp)[1]
+
+# get the posterior distribution of the 95th and 99th quantile for each site
+print("getting q95")
 set.71.95.q <- apply(yp, c(1, 2), quantile, probs = 0.95)
+print("getting q99")
 set.71.99.q <- apply(yp, c(1, 2), quantile, probs = 0.99)
 # set.71.95 <- apply(yp, c(2), quantile, probs=0.95)
 # set.71.99 <- apply(yp, c(2), quantile, probs=0.99)
 set.71.95 <- apply(set.71.95.q, 2, mean)
 set.71.99 <- apply(set.71.99.q, 2, mean)
 
+print("getting probs of exceedance")
 set.71.p.below <- matrix(0, np, nt)
 for (i in 1:np) { for (t in 1:nt) {
   set.71.p.below[i, t] <- mean(yp[, i, t] <= threshold)
